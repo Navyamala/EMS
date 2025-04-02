352b2ff865e4e88722cea4c84c81eee8877fabae
Navyamala <ramshyamutivelly@gmail.com>
2025-04-02 11:06:21 +0530

    Fix Vercel base path issue

--- employmanagement/vite.config.js
+++ employmanagement/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Fullstack_ProjectEMS/', // Make sure this matches your repo name
   build: {
     outDir: 'dist',
   },
